6e464ce165bef301fecab56431edd9d7fc49c5a1
jktravis <joshua.travis@gmail.com>
2016-04-19 12:26:39 -0400

    Calculating and setting status on an interval.

--- src/js/components/board.js
+++ src/js/components/board.js
@@ -2,6 +2,7 @@
 
 var React = require('react');
 var Cell = require('./cell');
+var _ = require('lodash');
 
 //noinspection JSUnusedGlobalSymbols
 var Board = React.createClass({
@@ -30,6 +31,83 @@ var Board = React.createClass({
     return grid;
   },
 
+  calculateStatus: function () {
+    let grid = this.state.grid;
+    for (let i = 0; i < grid.length; i++) {
+      let neighbors = this.getCellNeighbors(grid[i]);
+      let aliveNeighbors = 0;
+      let alive = _.includes(grid[i].status, 'alive');
+      let old = _.includes(grid[i].status, 'old');
+      for (let j = 0; j < neighbors.length; j++) {
+        if (neighbors[j] && _.includes(neighbors[j].status, 'alive')) {
+          aliveNeighbors = aliveNeighbors + 1;
+        }
+      }
+
+      if (alive) {
+        if (aliveNeighbors < 2) {
+          grid[i].status = [];
+        } else if (aliveNeighbors === 2 || aliveNeighbors === 3 && !old) {
+          grid[i].status.push('old');
+        } else if (aliveNeighbors > 3) {
+          grid[i].status = [];
+        }
+      } else if (aliveNeighbors === 3) {
+        grid[i].status.push('alive');
+      }
+    }
+    this.setState({grid: grid});
+  },
+
+  getCellNeighbors: function (cell) {
+    let topLeft, top, topRight,
+      left, right,
+      bottomLeft, bottom, bottomRight;
+
+    topLeft = this.state.grid.filter(function (o) {
+      return o.id === 'y=' + (cell.y - 1) + 'x=' + (cell.x - 1);
+    })[0];
+
+    top = this.state.grid.filter(function (o) {
+      return o.id === 'y=' + (cell.y - 1) + 'x=' + (cell.x);
+    })[0];
+
+    topRight = this.state.grid.filter(function (o) {
+      return o.id === 'y=' + (cell.y - 1) + 'x=' + (cell.x + 1);
+    })[0];
+
+    left = this.state.grid.filter(function (o) {
+      return o.id === 'y=' + (cell.y) + 'x=' + (cell.x - 1);
+    })[0];
+
+    right = this.state.grid.filter(function (o) {
+      return o.id === 'y=' + (cell.y) + 'x=' + (cell.x + 1);
+    })[0];
+
+    bottomLeft = this.state.grid.filter(function (o) {
+      return o.id === 'y=' + (cell.y + 1) + 'x=' + (cell.x - 1);
+    })[0];
+
+    bottom = this.state.grid.filter(function (o) {
+      return o.id === 'y=' + (cell.y + 1) + 'x=' + (cell.x);
+    })[0];
+
+    bottomRight = this.state.grid.filter(function (o) {
+      return o.id === 'y=' + (cell.y + 1) + 'x=' + (cell.x + 1);
+    })[0];
+
+    return [
+      topLeft,
+      top,
+      topRight,
+      left,
+      right,
+      bottomLeft,
+      bottom,
+      bottomRight
+    ];
+  },
+
   generateInitialStatus: function () {
     let num = Math.floor((Math.random() * 3));
     switch (num) {
@@ -46,6 +124,17 @@ var Board = React.createClass({
 
   componentWillMount: function () {
     this.setState({grid: this.generateGrid()});
+  },
+
+  componentDidMount: function () {
+    this._timer = setInterval(
+      () => this.calculateStatus(),
+      500
+    );
+  },
+
+  componentWillUnmount: function () {
+    clearInterval(this._timer);
   },
 
   render: function render() {
